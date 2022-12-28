a223029935d3c21ab6bbc6b5f1def694265b76a8
KaiquePacheco <kaique12br@gmai.com>
2022-12-27 22:49:44 -0300

    Integrating the postgres database, with the task table.

---
+++ src/db.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const {Sequelize, Model, DataTypes} = require("sequelize")
+
+const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`)
+
+var TaskModel = sequelize.define('Tasks', {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  team_name: DataTypes.STRING,
+  description: DataTypes.STRING(511),
+  start_date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  final_date: DataTypes.DATE,
+  is_finished: DataTypes.BOOLEAN
+}, {
+  timestamps: false
+})
+
+sequelize.authenticate()
+TaskModel.sync({alter: true})
+
+module.exports = {TaskModel, sequelize}
